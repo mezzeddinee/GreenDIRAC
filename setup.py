9da8234dcba7e324a2234181a764852941a9d326
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2025-10-06 15:53:08 +0200

    Setting up the project building procedure

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="greendirac",                   # Package name on PyPI
-    version="0.0.1",
-    author="CPPM/CNRS",
-    author_email="atsareg@in2p3.fr",
-    description="GreenDIGIT extension to DIRAC",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/GreenDIGIT-project/GreenDIRAC",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.11",
-)
+setup()
