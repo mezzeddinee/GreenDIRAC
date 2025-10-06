26af719bb0dc33ec5185569f0ab3c5e1ab09f5bd
Andrei Tsaregorodtsev <atsareg@in2p3.fr>
2025-10-06 14:10:06 +0200

    feat: setting up PyPi deployment

--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@ setup(
     name="GreenDIRAC",                   # Package name on PyPI
     version="0.1.0",
     author="Your Name",
-    author_email="you@example.com",
-    description="A short description of your package",
+    author_email="atsareg@in2p3.fr",
+    description="GreenDIGIT extension to DIRAC",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/GreenDIGIT-project/GreenDIRAC",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.11",
